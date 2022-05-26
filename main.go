f175e5a163338a018f3675baa14144dff5a95298
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2022-05-26 14:41:03 +0430

    handle func and add form v index .html

--- main.go
+++ main.go
@@ -8,10 +8,14 @@ import (
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w,"ParseForm() err: %v" , err)
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf()
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
 
 }
 
@@ -30,8 +34,8 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileserver)
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
