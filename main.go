49a016f4772bf3ac421439ba54b6b66b994e08d3
AmirAbbas Khoshbayan <amirabas.khoshbayan@gmail.com>
2022-07-06 17:43:42 +0430

    issue

--- main.go
+++ main.go
@@ -39,6 +39,9 @@ func main() {
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
+	v := "test"
+ fmt.Println(v)
+
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
